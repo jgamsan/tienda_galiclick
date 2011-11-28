ad446604709e711726a70cc0082a6da45ebc8c5a
Jose Miguel Gamazo <info@miproyectoweb.net>
2011-11-28 21:01:07 +0100

    modified initializer file

--- Gemfile
+++ Gemfile
@@ -17,7 +17,10 @@ group :assets do
 end
 
 gem 'jquery-rails'
-gem 'therubyracer'
+group :production do
+  gem 'therubyracer'
+end
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,8 @@ namespace :deploy do
   #  run "touch #{File.join(current_path,'tmp','restart.txt')}"
   #end
 end
-
+after 'deploy:update_code' do
+  run "cd /aplicacion_web/public_html/#{application}; RAILS_ENV=production rake assets:precompile"
+end
 after "deploy", "deploy:cleanup"
 require 'capistrano-unicorn'
