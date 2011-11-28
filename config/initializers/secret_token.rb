848997571832c82da239b67d164f009f054ff53b
Jose Miguel Gamazo <info@miproyectoweb.net>
2011-11-28 18:17:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TiendaGaliclick::Application.config.secret_token = 'ffdf030ee6e14efd6d77c74981aab58a3dda2d8e181ec1ad06833117458983b42dd3ec8c79c25b71c8ee3fcb2c34f5fc42148b3c406ac96540e8e4cec7be9abd'
