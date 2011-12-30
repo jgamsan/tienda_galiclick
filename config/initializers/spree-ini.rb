4a435c4c0d16f72149de5274f7b63e62e610521a
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2011-12-30 20:21:49 +0100

    Modified initial configuration

--- config/initializers/spree-ini.rb
+++ config/initializers/spree-ini.rb
@@ -1,2 +1,3 @@
 Spree::Config.set(:allow_ssl_in_production => false)
 Spree::Config.set(:default_locale => :es)
+Spree::Config.set(:site_name => "Galiclick 2.0 - Tu Tienda Online de Marcas Blancas")
